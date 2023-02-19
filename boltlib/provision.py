362659336d96b741ce4a146af7e559265c666229
Titusz Pan <titusz.pan@gmail.com>
2023-02-19 17:09:26 +0100

    implement wipe

--- boltlib/provision.py
+++ boltlib/provision.py
@@ -36,3 +36,26 @@ def provision(url, keys):
     writer.write(bl.burn_05_configure_picc(session, url))
     log.debug("Change KEYS")
     writer.write(bl.burn_06_change_keys(session, keys))
+
+
+def wipe(keys):
+    # type: (list[str]) -> None
+    """Wipe BoltRing/BoltCard"""
+
+    card = bl.wait_for_card()
+    writer = NFCWriter(card)
+
+    log.debug("Authenticate")
+    session = bl.AuthSession(keys[0])
+    response = writer.write(bl.wipe_01_auth_challenge())
+    response = writer.write(bl.wipe_02_auth_response(session, response.hex))
+    bl.wipe_03_auth_finalize(session, response.hex)
+
+    log.debug("Reset PICC")
+    writer.write((bl.wipe_04_reset_picc(session)))
+
+    log.debug("Reset Keys")
+    writer.write(bl.wipe_05_changekeys(session, keys))
+
+    log.debug("Clear NDEF")
+    writer.write(bl.wipe_06_clear_ndef())
