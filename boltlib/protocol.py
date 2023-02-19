cab2736b9c6c145fa50426a03bf9d297c10a21c0
Thilo Hille <hille@equinoxe.info>
2023-02-19 15:53:15 +0100

    90% of wipe-fuctions

--- boltlib/protocol.py
+++ boltlib/protocol.py
@@ -12,6 +12,12 @@ __all__ = [
     "burn_04_auth_finalize",
     "burn_05_configure_picc",
     "burn_06_change_keys",
+    "wipe_01_auth_challenge",
+    "wipe_02_auth_response",
+    "wipe_03_auth_finalize",
+    "wipe_04_reset_picc",
+    "wipe_05_changekeys",
+    "wipe_06_clear_ndef",
 ]
 
 DEFAULT_KEY = "00000000000000000000000000000000"
@@ -212,28 +218,130 @@ def burn_06_change_keys(session, keys):
 
 def wipe_01_auth_challenge(session):
     # type: (bl.AuthSession) -> list[str]
-    pass
+    session.key_enc = ""
+    session.key_mac = ""
+    session.ti = ""
+    session.cmd_counter = 0
+    return [APDU_SELECT_NTAG_424, APDU_AUTH_FIRST_PART_1]
 
 
-def wipe_02_auth_response(session, response):
+def wipe_02_auth_response(session, key, response):
     # type: (bl.AuthSession, str) -> list[str]
-    pass
+    """
+    Create ADPU commands to respond to auth challenge.
+
+    Note: will set rnd_a & rnd_b properties on `AuthSession`
+
+    :param AuthSession session: AuthSession object
+    :param str response: The response from burn_02 command
+    :return: List of APDUs
+    """
+    # Decrypt Challenge - The challenge is the 16 byte RND_B from PICC
+    IVbytes = b"\x00" * 16
+    key = bytes.fromhex(key)
+    cipher = AES.new(key, AES.MODE_CBC, IVbytes)
+    response = bytearray(bytes.fromhex(response[:-4]))
+    rnd_b = cipher.decrypt(response)
+
+    # Answer challenge with our own secret (RND_A) + rotated RND_B
+    print(rnd_b.hex())
+    rnd_b_rot = bl.rotate_bytes(rnd_b, -1)
+    rnd_a = session.rnd_a
+    answer = rnd_a + rnd_b_rot
+    cipher = AES.new(key, AES.MODE_CBC, IVbytes)
+    encrypted_answer = cipher.encrypt(answer)
+    prefix = b"\x90\xAF\x00\x00\x20"
+    postfix = b"\x00"
+    apdu = bytearray(prefix + encrypted_answer + postfix).hex().upper()
+    session.rnd_a = rnd_a
+    session.rnd_b = rnd_b
+    return [apdu]
 
 
-def wipe_03_auth_finalize(session, response):
+def wipe_03_auth_finalize(session, key, response):
     # type: (bl.AuthSession, str) -> None
-    pass
+    """
+    Finalize `AuthSession` object
+
+    Note: will set `key_enc`, `key_mac` and `ti` properties on `AuthSession`
+
+    :param AuthSession session: AuthSession object
+    :param response: Response from burn_03 command
+    """
+    IVbytes = b"\x00" * 16
+    key = bytes.fromhex(key)
+    cipher = AES.new(key, AES.MODE_CBC, IVbytes)
+    response = bytearray(bytes.fromhex(response[:-4]))
+    decrypted_auth_response = cipher.decrypt(response)
+    session.ti = decrypted_auth_response[:4]
+    session.key_enc, session.key_mac = bl.derive_session_keys(
+        key, session.rnd_a, session.rnd_b
+    )
+
 
 
 def wipe_04_reset_picc(session):
     # type: (bl.AuthSession) -> list[str]
-    pass
+    # type: (bl.AuthSession, str) -> list[str]
+    """
+    Configure PICC mirroring, SUN Messaging and other stuff
+    """
+    url_obj = ""
+    prefix = b"\x90\x5F\x00\x00\x19"
+    dataheader = b"\x02"  # Filenumber
+    filesettings = b"\x00\xE0\xEE"
+    encrypted_filesettings = bl.encrypt_data(session, filesettings)
+    cmacin = (
+        b"\x5f"
+        + session.cmd_counter_bytes
+        + session.ti
+        + dataheader
+        + encrypted_filesettings
+    )
+    encrypted_filesettings += bl.cmac_short(session.key_mac, cmacin)
+    postfix = b"\x00"
+    apdu = (prefix + dataheader + encrypted_filesettings + postfix).hex().upper()
+    session.cmd_counter += 1
+    return [apdu]
 
 
 def wipe_05_changekeys(session, keys):
     # type: (bl.AuthSession, list[str]) -> list[str]
-    pass
-
+    """Change Keys"""
+    assert len(keys) == 5, "Exactly 5 keys required"
+    assert session.authenticated, "Authenticated session required"
+    apdus = []
+    newkey = bytes.fromhex(DEFAULT_KEY)
+    key_no = 4
+    for key in reversed(keys):
+        prefix = b"\x90\xc4\x00\x00"
+        currentkey = bytes.fromhex(key)
+        keyversion = b"\x01"
+        if key_no != 0:
+            xorkey = bl.xor(newkey, currentkey)
+            keycrc32 = bl.jam_crc32(newkey)
+            print(f"\nCRC: {keycrc32.hex().upper()}")
+            keydata = xorkey + keyversion + keycrc32
+            print(f"\nKEYDATA: {keydata.hex().upper()}")
+        else:
+            keydata = newkey + keyversion
+        dataheader = key_no.to_bytes(1, "little", signed=False)
+        encrypted_keydata = bl.encrypt_data(session, keydata)
+        cmacin = (
+            b"\xc4"
+            + session.cmd_counter_bytes
+            + session.ti
+            + dataheader
+            + encrypted_keydata
+        )
+        encrypted_keydata += bl.cmac_short(session.key_mac, cmacin)
+        print(encrypted_keydata.hex())
+        commandpayload = dataheader + encrypted_keydata + b"\x00"
+        le = len(commandpayload[:-1]).to_bytes(1, "little", signed=False)
+        apdus.append((prefix + le + commandpayload).hex().upper())
+        session.cmd_counter += 1
+        key_no -= 1
+    return apdus
 
 def wipe_06_clear_ndef():
     # type: () -> list[str]
--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -69,3 +69,57 @@ def test_burn_06_change_keys(session):
     for e, a in zip(expected, apdus):
         assert a == e
     assert session.cmd_counter == 6
+
+
+def test_wipe_01_auth_challenge(session):
+    apdus = bl.wipe_01_auth_challenge(session)
+    assert apdus == ["00A4040007D276000085010100", "9071000005000300000000"]
+
+
+def test_wipe_02_auth_response(session):
+    session.rnd_a = bytes.fromhex("7FA9B3E2113552CFF1F44B8BB2B6775A")
+    response = "876FA92BBBBC15F470F28FF3851F373491AF"
+    apdus = bl.wipe_02_auth_response(session, keys[0], response)
+    assert apdus == [
+        "90AF000020047C98FFD117092937C798A603417B09324FDB66FF0B60356A65D8BF34B4152000"
+    ]
+    assert session.rnd_b == bytes.fromhex("95BA5DA5716557DBE86ED0A51C8EAD96")
+    assert session.cmd_counter == 0
+    assert session.authenticated is False
+
+
+def test_wipe_03_auth_finalize(session):
+    response = "EF58F86DA9CD541F3C280DC0C64385B77D4A064FCF8C77AB9A7C98144745E4509100"
+    bl.wipe_03_auth_finalize(session, keys[0], response)
+    assert session.rnd_a.hex().upper() == "7FA9B3E2113552CFF1F44B8BB2B6775A"
+    assert session.rnd_b.hex().upper() == "95BA5DA5716557DBE86ED0A51C8EAD96"
+    assert session.ti.hex().upper() == "C521C846"
+    assert session.key_mac.hex().upper() == "56DF52877E4B0FFD1159090811C279BE"
+    assert session.key_enc.hex().upper() == "7739D803B4304C69B60AC69A9ECB78C3"
+    assert session.cmd_counter == 0
+
+
+def test_wipe_04_reset_picc(session):
+    assert session.cmd_counter == 0
+    apdus = bl.wipe_04_reset_picc(session)
+    assert apdus == ["905F00001902DCEDE38C556531EEFACE37109948FD16B0656D366997A9C100"]
+    assert session.cmd_counter == 1
+
+
+def test_wipe_05_changekeys(session):
+    assert session.cmd_counter == 1
+    apdus = bl.wipe_05_changekeys(session, keys)
+    expected = [
+        "90C400002904125B7201D5FBC4D3550E751E66953B06BABB543F4CB8E270F5CFD208EEDAC2528A3485001F9F8BFD00",
+        "90C400002903D49BC9F646CE0E6CFB906127372B395DC59BD575660D015277CA78D54022CD5B1AFE77E104A8312900",
+        "90C400002902A64DC1429C5C9FB81E832A493B8153C73C1D3464468B34D360A600728F10C41F3A3377B54AF4C38100",
+        "90C400002901A11AD8CAA19332A82050D07B6DA8578C03905E6F61143F2DBF32FB34B60C3027A20DFE79F1CB3BF300",
+        "90C400002900731818AD6CCB4F6392AB4B6DBC15C306785E5920A53BB44BB9843C5F0C2BE059AC043FF5951BD2FF00",
+    ]
+    for e, a in zip(expected, apdus):
+        assert a == e
+    assert session.cmd_counter == 6
+
+
+def wipe_06_clear_ndef():
+    pass
